9e919ab33be940433232e500afc04d43032c875c
UmarAdkhamjonov <umarisystem@gmail.com>
2021-10-07 20:27:51 +0500

    alssfdf

---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using bootcamp.Solutions;
+
+namespace bootcamp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem21();
+        } 
+
+        static void Main20()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem20();
+        } 
+
+        static void Main19()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem19();
+        } 
+
+        static void Main18()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem18();
+        } 
+
+        static void Main17()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem17();
+        } 
+
+        static void Main16()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem16();
+        } 
+
+        static void Main15()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem15();
+        } 
+
+        static void Main14()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem14();
+        } 
+
+        static void Main13()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem13();
+        } 
+
+        static void Main12()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem12();
+        }  
+
+        static void Main11()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem11();
+        }  
+
+        static void Main10()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem10();
+        }  
+
+        static void Main9()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem9();
+        }  
+
+        static void Main8()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem8();
+        }  
+
+        static void Main7()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem7();
+        }  
+
+        static void Main6()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem6();
+        }  
+
+        static void Main5()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem5();
+        }  
+
+        static void Main4()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem4();
+        }  
+
+        static void Main3()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem3();
+        }   
+
+        static void Main2()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem2();
+        }
+
+        static void Main1()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem1();
+        }
+    }
+}
